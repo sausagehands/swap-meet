db05dcb16fa1d33ebfc26de7ffe90f1169a72b05
Maya Zakharova <mayzakharova@gmail.com>
2025-09-29 20:38:55 -0500

    add wave2

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,2 +1,12 @@
+import uuid
+
 class Item:
-    pass
+    def __init__(self, id = None):
+        if id is None:
+            self.id = uuid.uuid4().int
+        else:
+            self.id = id
+
+    def get_category(self):
+        return self.__class__.__name__
+    
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -14,3 +14,10 @@ class Vendor:
         
         self.inventory.remove(item)
         return item
+    
+    def get_by_id(self, id):
+        for item in self.inventory:
+            if item.id == id:
+                return item
+        return None    
+        
