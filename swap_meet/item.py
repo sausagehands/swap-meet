3c0c18d11f3d882d715e3238b8f10260b0af24e6
Bianca <myname45@gmail.com>
2025-09-30 08:35:54 -0400

    wave 3

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -10,3 +10,7 @@ class Item:
     def get_category(self):
         return self.__class__.__name__
     
+    def __str__(self):
+        return (f"An object of type Item with id {self.id}.")
+    
+        
--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -20,4 +20,18 @@ class Vendor:
             if item.id == id:
                 return item
         return None    
+    
+    def swap_items(self, other_vendor, my_item, their_item):
+        if my_item in self.inventory and their_item in other_vendor.inventory:
+            self.remove(my_item)
+            other_vendor.add(my_item)
+            
+            other_vendor.remove(their_item)
+            self.add(their_item)
+            
+            return True
+            
+        return False
+        
+        
         
